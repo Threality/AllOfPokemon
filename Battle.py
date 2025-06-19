ea04fbf637319b14ad161220d23ae485a997117e
Threality <finedoran@gmail.com>
2025-06-19 21:37:00 +0100

    Added creating pokemon

--- Battle.py
+++ Battle.py
@@ -11,3 +11,26 @@ class GameEvent:
     STATUS_GAINED = "statusGained"
     ON_STAT_CALC = "onStatCalc"
     ON_MEGA_EVOLVE = "onMegaEvolve"
+
+class Battle:
+    def __init__(self, team1, team2, battleType):
+        self._team1 = team1
+        self._team2 = team2
+        self._battleType = battleType
+        self._weather = None
+        self._weatherDuration = 0
+        self._terrain = None
+        self._terrainDuration = 0
+
+    def SetTerrain(self, terrain, pokemon):
+        self._terrain = terrain
+        self._terrainDuration = 5
+        pokemon._item.HandleEvent("terrainChange", pokemon, self, terrain=terrain) # checks if extra duration needs to be added
+
+    def SetWeather(self, weather, pokemon):
+        self._weather = weather
+        self._weatherDuration = weather._duration
+        pokemon._item.HandleEvent("weatherChange", pokemon, self, weather=weather) # checks if extra duration needs to be added
+
+    class Turn:
+        pass
